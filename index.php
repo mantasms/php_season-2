dd356499718bf6b6dcfcf4f261e9720e216f4970
mantasms <m.sakaitis@gmail.com>
2019-03-19 11:58:01 +0200

    OOP uzduotis nr3

--- index.php
+++ index.php
@@ -2,11 +2,18 @@
 
 class ThailandSurprise {
 
+    public $clothes;
     private $balls;
 
     public function __construct() {
         $this->balls = rand(0, 1);
     }
+    
+    public function attachBalls() {
+        
+    }
+    
+    public function detachBalls (){
 
 }
 
