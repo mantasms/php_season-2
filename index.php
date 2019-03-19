1a2684c109dd3f60068b43369ef9d5feeb98dfd4
mantasms <m.sakaitis@gmail.com>
2019-03-19 12:40:30 +0200

    OOP uzduotis nr5

--- index.php
+++ index.php
@@ -17,7 +17,24 @@ class ThailandSurprise {
         $this->balls = false;
     }
 
+    public function getPhoto() {
+        if ($this->balls) {
+            return 'http://catplanet.org/wp-content/uploads/2014/04/Look-at-my-fucking-balls.jpg';
+        } else {
+            return 'http://rlv.zcache.com/no_ball_games_thai_park_sign_posters-rc2d0581729404d5a8c1fc49e07142f27_wxt_8byvr_324.jpg';
+        };
+    }
+
 }
 
 $surprise = new ThailandSurprise();
+
 ?>
+<html>
+    <head>
+        <title> OOP </title>
+    </head>
+    <body>
+        <img src="<?php print $surprise->getPhoto(); ?>">
+    </body>
+</html>
