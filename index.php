4dd6701a1574246cc8cb5ae9c33fb8b4f825b221
mantasms <m.sakaitis@gmail.com>
2019-03-20 12:45:53 +0200

    OOP diena 2 - Get Set uzduotis nr1 v2

--- index.php
+++ index.php
@@ -14,27 +14,27 @@ class Gerimas {
     }
 
     public function setName(string $name) {
-        $this->name = $name;
+        $this->data['name'] = $name;
     }
 
     public function getName(string $name) {
-        $this->name = $name;
+        $this->data['name'] = $name;
     }
 
     public function setAmountml(int $amount_ml) {
-        $this->amount_ml = $amount_ml;
+        $this->data['amount_ml'] = $amount_ml;
     }
 
     public function getAmountml(int $amount_ml) {
-        $this->amount_ml = $amount_ml;
+        $this->data['amount_ml'] = $amount_ml;
     }
 
     public function setAbarot(float $abarot) {
-        $this->abarot = $abarot;
+        $this->data['abarot'] = $abarot;
     }
 
     public function getAbarot(float $abarot) {
-        $this->abarot = $abarot;
+        $this->data['abarot'] = $abarot;
     }
 
 }
