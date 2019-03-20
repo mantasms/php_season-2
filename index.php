ad4cec00ca45d28aee78b446af7cf739083b1bad
mantasms <m.sakaitis@gmail.com>
2019-03-20 13:15:14 +0200

    OOP diena 2 - Get Set uzduotis nr3

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-declare(striuct_types = 1);
+declare(strict_types = 1);
 
 class Gerimas {
 
@@ -9,40 +9,53 @@ class Gerimas {
         $this->data = [
             'name' => null,
             'amount_ml' => null,
-            'abarot' => float
+            'abarot' => null
         ];
+    }
+
+    public function getName() {
+        return $this->data['name'];
     }
 
     public function setName(string $name) {
         $this->data['name'] = $name;
     }
 
-    public function getName(string $name) {
-        $this->data['name'] = $name;
+    public function getAmountMl() {
+        return $this->data['amount_ml'];
     }
 
-    public function setAmountml(int $amount_ml) {
-        $this->data['amount_ml'] = $amount_ml;
+    public function setAmountMl(int $amount) {
+        $this->data['amount_ml'] = $amount;
     }
 
-    public function getAmountml(int $amount_ml) {
-        $this->data['amount_ml'] = $amount_ml;
+    public function getAbarot() {
+        return $this->data['abarot'];
     }
 
     public function setAbarot(float $abarot) {
         $this->data['abarot'] = $abarot;
     }
 
-    public function getAbarot(float $abarot) {
-        $this->data['abarot'] = $abarot;
+    public function getData() {
+        return $this->data;
+    }
+
+    public function setData(array $data) {
+        $this->setName($data['name'] ?? null);
+        $this->setAmountMl($data['amount_ml'] ?? null);
+        $this->setAbarot($data['abarot'] ?? null);
     }
 
 }
 
+$alus = new Gerimas();
+$alus->setName('Kanapinis');
+print $alus->getName();
 ?>
 <html>
     <head>
-        <title> OOP </title>
+        <title> PHP Seasson#2 OOP </title>
     </head>
     <body>
         <p>
