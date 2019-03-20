ea0b78c17d47a13cdc4a7f57c8c16e4e7b30f017
mantasms <m.sakaitis@gmail.com>
2019-03-20 10:52:06 +0200

    OOP diena 2 - uzduotis nr1

--- index.php
+++ index.php
@@ -1,42 +1,20 @@
 <?php
 
-class ThailandSurprise {
+class Jacuzzi {
 
-    public $clothes;
-    private $balls;
-    private $name;
+    public $amount_water;
+    public $amount_non_water;
 
-    public function __construct($name) {
-        $this->name = $name;
-        $this->balls = rand(0, 1);
+    public function __construct($amount_water = 0, $amount_non_water = 0) {
+        $this->amount_water = $amount_water;
+        $this->amount_non_water = $amount_non_water;
     }
-
-    public function attachBalls() {
-        $this->balls = true;
-    }
-
-    public function detachBalls() {
-        $this->balls = false;
-    }
-
-    public function getPhoto() {
-        if ($this->balls) {
-            return 'http://catplanet.org/wp-content/uploads/2014/04/Look-at-my-fucking-balls.jpg';
-        } else {
-            return 'http://rlv.zcache.com/no_ball_games_thai_park_sign_posters-rc2d0581729404d5a8c1fc49e07142f27_wxt_8byvr_324.jpg';
-        };
-    }
-
-}
-
-$surprise = new ThailandSurprise('mazafaka');
-
-?>
+    
+    ?>
 <html>
     <head>
         <title> OOP </title>
     </head>
     <body>
-        <img src="<?php print $surprise->getPhoto(); ?>">
     </body>
 </html>
